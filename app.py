3014a494bd8e05f064c127383299dec06f255af5
snehalvispute16 <154119925+snehalvispute16@users.noreply.github.com>
2024-04-20 02:24:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+import google.generativeai as genai
+import streamlit as st
+
+# Read Gemini API key from file
+f = open(r"Gemini_API_KEY.txt")
+key = f.read()
+
+# Configure Gemini API
+genai.configure(api_key=key)
+
+# Set Streamlit app title and subheader
+st.title("DataMentor 📝")
+# st.subheader("AI Conversational Tutor")
+
+# A note about what the app does
+st.markdown("""
+This app is an **'AI Conversational Tutor'** designed to assist with data science-related questions. Simply type your question in the input box below, and the AI Tutor will provide a response based on its knowledge.
+""")
+
+# Initialize Gemini model
+model = genai.GenerativeModel(
+    model_name="gemini-1.5-pro-latest",
+    system_instruction="You are a helpful AI Teaching Assistant. Politely provide answers to user queries related to data science topics and provide helpful insights. If the query is unrelated, respond with 'I'm sorry, I'm not able to assist with that topic at the moment. However, if you have any other questions or need help with data science-related topics, feel free to ask!' If the user says 'hi', respond with 'Hi! How can I help you?'"
+)
+
+# Initialize chat history
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
+
+# Dropdown menu for topic selection
+topic_options = ["Data Science", "Machine Learning", "Data Visualization", "Statistics"]
+selected_topic = st.selectbox("Select a topic:", topic_options)
+
+
+# Start chat with model
+chat = model.start_chat(history=st.session_state['chat_history'])
+
+# Display chat history
+for msg in chat.history:
+    st.chat_message(msg.role).write(msg.parts[0].text)
+
+# Get user input
+user_prompt = st.chat_input()
+
+# Process user input
+if user_prompt:
+    st.chat_message("user").write(user_prompt)
+    response = chat.send_message(user_prompt)
+    st.chat_message("ai").write(response.text)
+    st.session_state["chat_history"] = chat.history
